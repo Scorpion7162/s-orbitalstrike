2213b422d2d2df707f3038f3a486bfcef7f87a4c
Scorpion <144194359+Scorpion7162@users.noreply.github.com>
2025-06-22 15:48:29 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 local function orbitalstrike(coords)
     for i = 1, Config.Iterations do
         AddExplosion(coords.x, coords.y, coords.z + 1.0, 29, 10.0, true, false, 10.0)
-        Wait(250)
+        Wait(Config.WaitTime)
     end
  
     UseParticleFxAssetNextCall('scr_xm_orbital')
